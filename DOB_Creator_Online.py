265d083481f5ca14804aca3e5ea9f626d7d96f46
Alfred <alfred@alfred.local>
2026-06-30 12:44:13 -0400

    Rename app to Astom-DCOT

--- DOB_Creator_Online.py
+++ DOB_Creator_Online.py
@@ -3,10 +3,10 @@ import streamlit as st
 
                    
 st.set_page_config(
-    page_title="ALSTOM-DCO",
+    page_title="Astom-DCOT",
     layout="wide",)
 
-st.header('ALSTOM-DCO')
+st.header('Astom-DCOT')
 
 with st.form("my_form"): 
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
