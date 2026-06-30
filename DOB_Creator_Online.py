850f10205ff7270501ea9e0a7fe7185490202035
Alfred <alfred@alfred.local>
2026-06-30 12:07:01 -0400

    Rename app to ALSTOM-DCO

--- DOB_Creator_Online.py
+++ DOB_Creator_Online.py
@@ -3,10 +3,10 @@ import streamlit as st
 
                    
 st.set_page_config(
-    page_title="DOB Package Creator",
+    page_title="ALSTOM-DCO",
     layout="wide",)
 
-st.header('DOB package Creator')
+st.header('ALSTOM-DCO')
 
 with st.form("my_form"): 
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
